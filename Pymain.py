9c33717c4fa67515db719eb6378ac68a9f743972
Evan Le Chenadec <evan.lechenadec@entpe.fr>
2025-11-06 10:58:09 +0100

    Clean up due to the creation of the others repos.
    Signed-off-by: Evan Le Chenadec <evan.lechenadec@entpe.fr>

--- Pymain.py
+++ Pymain.py
@@ -1,4 +1,34 @@
-list_RNAs = ["UUU", "UUC", "UUA", "UUG", "CUU", "CUC", "CUA", "CUG","AUU", "AUCU", "AUA", "AUG", "GUU", "GUC", "GUG", "GUA",
+class DNA(list_DNA):
+    strand = ""
+    def __init__(self):
+        self.strand = str(input("Enter the strand here: s"))
+    def DNA_to_PRT (strand):
+        prt = []
+    #    if len(list_DNA)%3 != 0:
+    #        return "Incorrect Strand Size"
+        Ref_DNA_to_AA = dict(zip(strand, list_AAs))
+        for i in range(len(strand)-2):
+            prt.append(Ref_DNA_to_AA[strand[i]+strand[i+1]+strand[i+2]])
+        return prt
+    def DNA_to_RNA(list_DNA):
+        list_RNA = []
+        for i in range(len(list_DNA)):
+            if list_DNA[i] == "T":
+                list_RNA[i] = "U"
+            else:
+                list_RNA[i] = list_DNA[i]
+        return list_RNA
+    def ORF_search(list_DNA):
+        list_ORF = []
+        for i in range(len(list_DNA)-2):
+            if list_DNA[i:i+3] == 'ATG':
+                for j in range(i+3, len(list_DNA)-2, 3):
+                    if list_DNA[j:j+3] in ("TAA", "TAG", "TGA"):
+                        list_ORF.append(list_DNA[i:j+3])
+                        break
+        return list_ORF
+
+list_RNAs = ["UUU", "UUC", "UUA", "UUG", "CUU", "CUC", "CUA", "CUG","AUU", "AUC", "AUA", "AUG", "GUU", "GUC", "GUG", "GUA",
             "UCU", "UCC", "UCA", "UCG",  "CCU", "CCC", "CCA", "CCG", "ACU", "ACC", "ACA", "ACG", "GCU", "GCC", "GCG", "GCA",
             "UAU", "UAC", "UAA", "UAG", "CAU", "CAC", "CAA", "CAG", "AAU", "AAC", "AAA", "AAG", "GAU", "GAC", "GAG", "GAA",
             "UGU", "UGC", "UGA", "UGG", "CGU", "CGC", "CGA", "CGG", "AGU", "AGC", "AGA", "AGG", "GGU", "GGC", "GGG", "GGA"]
@@ -6,7 +36,7 @@ list_AAs = ["Phe", "Phe", "Leu", "Leu", "Leu", "Leu", "Leu", "Leu", "Ile", "Ile"
             "Ser", "Ser", "Ser", "Ser", "Pro", "Pro", "Pro", "Pro", "Thr", "Thr", "Thr", "Thr", "Ala", "Ala", "Ala", "Ala",
             "Tyr", "Tyr", "STP", "STP", "His", "His", "Gln", "Gln", "Asn", "Asn", "Lys", "Lys", "Asp", "Asp", "Glu", "Glu",
             "Cys", "Cys", "STP", "Trp", "Arg", "Arg", "Arg", "Arg", "Ser", "Ser", "Arg", "Arg", "Gly", "Gly", "Gly", "Gly"]
-list_DNAs = ["TTT", "TTC", "TTA", "TTG", "CTT", "CTC", "CTA", "CTG","ATT", "ATCT", "ATA", "ATG", "GTT", "GTC", "GTG", "GTA",
+list_DNAs = ["TTT", "TTC", "TTA", "TTG", "CTT", "CTC", "CTA", "CTG","ATT", "ATC", "ATA", "ATG", "GTT", "GTC", "GTG", "GTA",
               "TCT", "TCC", "TCA", "TCG",  "CCT", "CCC", "CCA", "CCG", "ACT", "ACC", "ACA", "ACG", "GCT", "GCC", "GCG", "GCA",
               "TAT", "TAC", "TAA", "TAG", "CAT", "CAC", "CAA", "CAG", "AAT", "AAC", "AAA", "AAG", "GAT", "GAC", "GAG", "GAA",
               "TGT", "TGC", "TGA", "TGG", "CGT", "CGC", "CGA", "CGG", "AGT", "AGC", "AGA", "AGG", "GGU", "GGC", "GGG", "GGA"]
@@ -21,6 +51,16 @@ def RNA_to_PRT (list_RNA):
     for i in range(len(list_RNA)-2):
         prt.append(Ref_RNA_to_AA[list_RNA[i]+list_RNA[i+1]+list_RNA[i+2]])
     return prt
+
+def DNA_to_PRT (list_DNA):
+    prt = []
+#    if len(list_DNA)%3 != 0:
+#        return "Incorrect Strand Size"
+    Ref_DNA_to_AA = dict(zip(list_DNAs, list_AAs))
+    for i in range(len(list_DNA)-2):
+        prt.append(Ref_DNA_to_AA[list_DNA[i]+list_DNA[i+1]+list_DNA[i+2]])
+    return prt
+
 def DNA_to_RNA(list_DNA):
     list_RNA = []
     for i in range(len(list_DNA)):
@@ -42,4 +82,6 @@ def ORF_search(list_DNA):
 with open("dummy.txt", 'r') as t:
     strand3 = t.read()
 
-print(ORF_search(strand3))
+orfs = ORF_search(strand3)
+prts = DNA_to_PRT((orfs))
+print(prts)
--- TODO.md
+++ TODO.md
@@ -3,8 +3,8 @@
 Stuff I will add in the future/need to do :
 
 1. [ ] Correcting the README.md
-2. [ ] Finishing the ORF_search function
-3. [ ] Finishing the DRNA_to_PRT functions
-4. [ ] Preparing the CMake files
+2. [X] Finishing the ORF_search function
+3. [X] Finishing the DRNA_to_PRT functions
+4. [ ] Making proper tests for the Python script
 5. [ ] Adding classes for DNA/RNA/PRT strands
 6. [ ] Refactoring the functions as methods of the according classes
